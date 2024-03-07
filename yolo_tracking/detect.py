46e839256845c8a0a4c9698adec289c69b533d89
Wong Yi Jie <55955482+yjwong1999@users.noreply.github.com>
2024-03-07 12:00:29 +0800

    Update detect.py

--- yolo_tracking/detect.py
+++ yolo_tracking/detect.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO('../pretrained_models/face_detection/yolov8_pytorch.pt')  # load a custom model
 
 # Predict with the model
-results = model('source.streams', show=True, vid_stride=2, device=0)  
+results = model('source.streams', show=True, vid_stride=3, device=0)  
